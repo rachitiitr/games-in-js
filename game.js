1d49fc395478336872e7000294f76b113e98266d
Learning Algorithms With Rachit Jain <32428330+rachitiitr@users.noreply.github.com>
2020-01-26 20:27:47 +0530

    summarizing the power of whatever built so far
    how easy to build the platform now and interact with it

--- game.js
+++ game.js
@@ -8,19 +8,23 @@ document.addEventListener('keydown', keyDownHandler);
 document.addEventListener('keyup', keyUpHandler);
 
 // player2 = new Blob(BLOB_RECT, 0, 0, 20, 20, 'cyan');
-player2 = new Blob(BLOB_CIRCLE, 40, 40, 20, 20, 'blue');
-ledge = new Ledge(10, 200, 200);
-ledge2 = new Ledge(15, 600, 250);
+player2 = new Blob(BLOB_CIRCLE, 40, 40, 20, 20, 'brown');
+ledges = [
+            new Ledge(10, 200, 200),
+            new Ledge(5, 600, 350),
+            new Ledge(9, 360, 550),
+            new Ledge(7, 40, 750),
+        ]
 
 
 function draw() {
     drawRect(0, 0, W, H, 'black');
     // player.update();
     player2.update();
-    ledge.update();
-    ledge.collide(player2);
-    ledge2.update();
-    ledge2.collide(player2);
+    ledges.forEach(ledge => {
+        ledge.update();
+        ledge.collide(player2);
+    });
 
     requestAnimationFrame(draw);
 }
