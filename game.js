cf8840027bfafdc70a23491cc64f4ba008b2cd25
Learning Algorithms With Rachit Jain <32428330+rachitiitr@users.noreply.github.com>
2020-01-26 18:01:26 +0530

    Create game.js

---
+++ game.js
@@ -0,0 +1,49 @@
+console.log('ok!')
+let canvas = document.querySelector('#cvGame')
+let ctx = canvas.getContext('2d');
+let W = canvas.getBoundingClientRect().width;
+let H = canvas.getBoundingClientRect().height;
+
+document.addEventListener('keydown', keyDownHandler);
+document.addEventListener('keyup', keyUpHandler);
+
+player = new Blob(BLOB_RECT, 0, 0, 10, 20, 'cyan');
+player2 = new Blob(BLOB_CIRCLE, 40, 40, 20, 20, 'blue');
+
+function draw() {
+    drawRect(0, 0, W, H, 'black');
+    drawText(100, 50, "Hello World", 'white');
+    player.update();
+    player2.update();
+
+    requestAnimationFrame(draw);
+}
+
+draw();
+
+
+
+function drawCircle(centerX, centerY, radius, fillColor) {
+    ctx.beginPath();
+
+    ctx.fillStyle = fillColor;
+    ctx.arc(centerX,centerY, radius, 0,Math.PI*2, true);
+    ctx.fill();
+
+    ctx.closePath();
+}
+
+function drawText(topLeftX, topLeftY, msg, fillColor='black', fontSize='3em') {
+    ctx.font = `${fontSize} Arial`;
+    ctx.fillStyle = fillColor
+    ctx.fillText(msg, topLeftX, topLeftY)
+}
+
+function keyDownHandler(e) {
+    player.keyDownHandler(e);
+    player2.keyDownHandler(e);
+}
+function keyUpHandler(e) {
+    player.keyUpHandler(e);
+    player2.keyUpHandler(e);
+}
