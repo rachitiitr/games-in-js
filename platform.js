9f9f9f7f5856209eda15c83b59396dcb99c8b4d9
Learning Algorithms With Rachit Jain <32428330+rachitiitr@users.noreply.github.com>
2020-01-26 19:38:21 +0530

    Adding class to handle ledges and platform

---
+++ platform.js
@@ -0,0 +1,28 @@
+class Ledge {
+    constructor(n, x, y, imgUrl='https://i.ibb.co/HXVKCrW/block.png') {
+        this.n = n;
+        this.space = {x, y};
+        this.rock = new Image;
+        this.rock.src = imgUrl;
+    }
+
+    update() {
+        for(let i=0; i<this.n; i++)
+            ctx.drawImage(this.rock, this.space.x+32*i, this.space.y)
+    }
+
+    collide(player) {
+        for(let i=0; i<this.n; i++) {
+            let curX = this.space.x+32*i;
+            let curY = this.space.y;
+            if (curX <= player.space.x && player.space.x <= curX+this.rock.width) {
+                let diff =  curY - player.space.y;
+                if(0 <= diff && diff < player.space.h) {
+                    player.speed.dy = 0;
+                    player.events.jumping.status = JUMPING_NOT;
+                    player.space.y = curY - player.space.h;
+                }
+            }
+        }
+    }
+}
